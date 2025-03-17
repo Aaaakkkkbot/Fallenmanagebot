c1a81a5cebf7fe09698832cccd5158922cb7d22d
ARAN アラン <115202562+sokuyo@users.noreply.github.com>
2025-03-17 15:31:00 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
+import os
 from flask import Flask
 from threading import Thread
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return "Bot is running!", 200
 
-def run_server():
-    app.run(host='0.0.0.0', port=8000)
+def run_web_server():
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
 
-server_thread = Thread(target=run_server)
-server_thread.start()
+# Start the web server in a separate thread
+Thread(target=run_web_server).start()
